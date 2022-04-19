ec20b5de38c4ce96ff32d7959fb2447db173c4d1
huongbiii <88549575+huongbiii@users.noreply.github.com>
2022-04-20 01:32:55 +0700

    commit

--- src/java/entity/Role.java
+++ src/java/entity/Role.java
@@ -10,5 +10,27 @@ package entity;
  * @author chubo
  */
 public class Role {
+    private int roleId;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+    
     
 }
--- src/java/entity/Users.java
+++ src/java/entity/Users.java
@@ -5,10 +5,103 @@
  */
 package entity;
 
+import java.sql.Date;
+
 /**
  *
  * @author chubo
  */
 public class Users {
+    private int userId;
+    private String role;
+    private String username;
+    private String email;
+    private String password;
+    private String fullName;
+    private Date birthDate;
+    private boolean gender;
+    private String phone;
+    private String address;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
     
 }
